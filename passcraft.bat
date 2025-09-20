35409db5340c6df8b3db568534cd8f3806897860
ephemeral8997 <mazen001.ahmed001@gmail.com>
2025-09-20 03:26:32 +0300

    Fix *.bat

--- passcraft.bat
+++ passcraft.bat
@@ -1,8 +1,11 @@
 @echo off
+set SCRIPT_DIR=%~dp0
+
 where python >nul 2>nul
 if errorlevel 1 (
     echo Python is not installed or not in PATH.
     exit /b 1
 )
 
-python passcraft.py %*
+
+python "%SCRIPT_DIR%passcraft.py" %*
